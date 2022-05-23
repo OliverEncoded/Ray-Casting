b8e56834ab6bac437f46ed6300a5a2e94bf32b53
Oliver_Encoded <86622891+OliverEncoded@users.noreply.github.com>
2022-05-23 09:40:04 -0400

    Add files via upload

---
+++ Code/RenderCanvas.cpp
@@ -0,0 +1,84 @@
+#include "RenderCanvas.hpp"
+
+using namespace sfas;
+
+bool flag = true;
+    Ray::Ray(sf::Vector2f origin, sf::Vector2f direction)
+    {
+        this->origin = origin;
+        this->direction = direction;
+        this->position = origin;
+    }
+    
+    bool Ray::move(float inc)
+    {
+        return false;
+    }
+
+
+    square::square(){}
+    square::square(sf::Vector2f position, sf::Vector2f size, sf::Color color)
+    {
+        this->position = position;
+        this->size = size;
+        this->color = color;
+    }
+
+
+void createRays(std::vector<Ray>& rays, float raySeperation, sf::Vector2i pos)
+{
+    rays.clear();
+    for(float x = -1; x <= 1; x += raySeperation)
+    {
+        for(float y = -1; y <= 1; y+= raySeperation)
+        {
+            rays.push_back(Ray(sf::Vector2f(pos.x,pos.y) , sf::Vector2f(x, y)));
+        }
+    }
+}
+
+void runRayPath(Ray ray, Canvas &c, std::vector<square> &squares, int rayLength, bool showRayPath, bool doReflections)
+{
+
+    for(int i = 0; i <= rayLength; i++){
+    ray.position += sf::Vector2f(ray.direction.x, ray.direction.y);
+        
+        sf::Vector2f rPos = ray.position;
+        
+        c.brushColor = sf::Color(80, 80, 80);
+        
+        if(showRayPath)
+        c.drawPixel(ray.position.x, ray.position.y);
+        
+        if(doReflections)
+        {
+        if(rPos.x >= 800)
+            ray.direction.x *= -1;
+        if(rPos.x <= 0)
+            ray.direction.x *= -1;
+        if(rPos.y >= 500)
+            ray.direction.y *= -1;
+        if(rPos.y <= 0)
+            ray.direction.y *= -1;
+        }
+        
+        bool collided = false;
+        for(int sq = 0; sq < squares.size(); sq++)
+        {
+            square &s = squares[sq];
+            sf::Vector2f sPos = s.position;
+        
+            
+        if(rPos.x > sPos.x && rPos.x < sPos.x + s.size.x && rPos.y > sPos.y && rPos.y < sPos.y + s.size.y)
+        {
+            c.brushColor = s.color;
+            c.drawPixel(ray.position.x, ray.position.y);
+            collided = true;
+            break;
+        }
+        }
+        if (collided)
+           break;
+    }
+}
+
